6093a5bca40b18e20c26751ca6ff7b352e40dbf2
Mr_Voracious <vanditkad@gmail.com>
2025-08-02 16:17:58 +0530

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -28,7 +28,7 @@ function sendEmail() {
     const bodyMsg = `<b><h4>Name</h3></b> ${fullName.value} <br> <b><h4>Email</h3></b> ${email.value} <br> <b><h4>Message</h4></b> ${msg.value}`;
 
     Email.send({
-        SecureToken: '9fecc46e-3a61-41dc-8571-dec54356a7be',
+        SecureToken: 'bc0696e9-6788-4b67-9886-b518005683c9',
         To: 'byteclub@pp.balbharati.org',
         From: "byteclub@pp.balbharati.org",
         Subject: `${fullName.value} would like to contact you`,
